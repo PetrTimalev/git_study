9d766d1ce68c176d628b27f77d59e7c9143dbc64
petrtimalev <petr.timalev@gmail.com>
2025-02-06 15:03:27 +1000

    init

---
+++ main.py
@@ -0,0 +1,15 @@
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+def summ():
+    print( 3 + 5)
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    summ()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
